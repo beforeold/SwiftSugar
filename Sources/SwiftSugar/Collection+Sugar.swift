c4f3c486568f38b5d5f3185633d756da7e4313a6
beforeold <ibeforeold@gmail.com>
2022-09-03 20:13:46 +0800

    add isNotEmpty

---
+++ Sources/SwiftSugar/Collection+Sugar.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by beforeold on 2022/9/3.
+//
+
+import Foundation
+
+extension Collection {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
