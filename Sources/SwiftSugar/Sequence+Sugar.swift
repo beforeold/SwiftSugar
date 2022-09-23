ca18b534c3d44bdf45c32b9e7b09e3f3d09460fd
beforeold <ibeforeold@gmail.com>
2022-09-23 21:50:31 +0800

    add componentsJoined(by:) for sequence

---
+++ Sources/SwiftSugar/Sequence+Sugar.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by beforeold on 2022/9/23.
+//
+
+import Foundation
+
+public extension Sequence {
+    /// join the sequence with seperator, for example transform array [1, 2, 3] into string "1,2,3"
+    func componentsJoined(by seperator: String = ",") -> String {
+        return map { "\($0)" }.joined()
+    }
+}
