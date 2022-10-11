2c298d22d079c2d064456bd20a727cc8465c3e21
beforeold <ibeforeold@gmail.com>
2022-10-11 23:58:57 +0800

    add Hashable+Sugar

---
+++ Sources/SwiftSugar/Hashable+Sugar.swift
@@ -0,0 +1,19 @@
+//
+//  Hashable+Sugar.swift
+//  
+//
+//  Created by beforeold on 2022/10/11.
+//
+
+import Foundation
+
+public extension Hashable where Self: AnyObject {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs === rhs
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+    
+}
