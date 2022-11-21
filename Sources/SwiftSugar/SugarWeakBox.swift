cb711ffdb0f806aedba4f305535f0fd183a470f1
beforeold <ibeforeold@gmail.com>
2022-11-21 20:22:30 +0800

    add SugarWeakBox

--- Sources/SwiftSugar/SugarWeakBox.swift
+++ Sources/SwiftSugar/SugarWeakBox.swift
@@ -1,15 +1,15 @@
 //
-//  SugarValueBox.swift
+//  SugarWeakBox.swift
 //  
 //
-//  Created by beforeold on 2022/11/20.
+//  Created by beforeold on 2022/11/21.
 //
 
 import Foundation
 
 /// a reference type box for a value, like Optional Enum,
-final class SugarValueBox<Value> {
-  public var value: Value?
+final class SugarWeakBox<Value: AnyObject> {
+  public weak var value: Value?
   
   public init(value: Value? = nil) {
     self.value = value
