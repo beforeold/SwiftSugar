17b7162de90628ec444fba3828be3cfaeda6909f
beforeold <ibeforeold@gmail.com>
2022-12-12 21:45:46 +0800

    add UnownedSugar

---
+++ Sources/SwiftSugar/UnownedSugar.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by beforeold on 2022/12/12.
+//
+
+import Foundation
+
+/// can be used as object on publisher.assignTo:on:
+public class UnownedSugar<T: AnyObject> {
+  unowned var value: T
+  
+  init(_ value: T) {
+    self.value = value
+  }
+}
